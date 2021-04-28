66c0274fac2ca6d830fafab53563dc96a5022007
Can Li <>
2021-04-28 12:52:41 -0400

    1

--- src/index.js
+++ src/index.js
@@ -1,19 +1,26 @@
 // Import the React and ReactDOM libraries
 import React from "react";
 import ReactDOM from "react-dom";
+import faker from 'faker';
 
 // Create a react component
 const App = () => {
-  const buttonTest = 'Click Me'
   return (
-    <div>
-      <label className="label" htmlFor="name">
-        Enter name:
-      </label>
-      <input id="name" type="text" />
-      <button style={{ backgroundColor: 'blue', color: 'white' }}>
-        {buttonTest}
-      </button>
+    <div className="ui container comments">
+      <div className="comment">
+        <a href="/" className="avatar">
+          <img alt="avatar" src={faker.image.avatar()} />
+        </a>
+        <div className="content">
+          <a href="/" className="author">
+            Sam
+          </a>
+          <div className="metadata">
+            <span className="date">Toda at 6:00PM</span>
+          </div>
+          <div className="text">Nice blog post!</div>
+        </div>
+      </div>
     </div>
   )
 };
