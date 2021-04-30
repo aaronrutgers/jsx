e56141bb487901f7709cce4ac8d788438a18fe4d
Can Li <>
2021-04-30 15:42:11 -0400

    1

--- src/index.js
+++ src/index.js
@@ -1,28 +1,16 @@
 // Import the React and ReactDOM libraries
 import React from "react";
 import ReactDOM from "react-dom";
-import faker from 'faker';
-
+import CommentDetail from "./CommentDetail";
 // Create a react component
 const App = () => {
   return (
     <div className="ui container comments">
-      <div className="comment">
-        <a href="/" className="avatar">
-          <img alt="avatar" src={faker.image.avatar()} />
-        </a>
-        <div className="content">
-          <a href="/" className="author">
-            Sam
-          </a>
-          <div className="metadata">
-            <span className="date">Toda at 6:00PM</span>
-          </div>
-          <div className="text">Nice blog post!</div>
-        </div>
-      </div>
+      <CommentDetail author="Sam" timeAgo="Today at 4:45PM" />
+      <CommentDetail author="Alex" timeAgo="Today at 2:00AM" />
+      <CommentDetail author="Jane" timeAgo="Today at 6:00pm" />
     </div>
-  )
+  );
 };
 
 // Take the react component and show it on the screen
